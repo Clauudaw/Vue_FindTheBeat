19c8e1cc941f6fd4063e6e5eb3def974fda4e1ff
Clauudaw <claudiardaw@gmail.com>
2024-11-27 16:46:09 +0100

    install bootstrap vue 3

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+
+// Bootstrap CSS y JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// BootstrapVue 3
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 app.use(createPinia())
