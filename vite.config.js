5413acabbb6447bd2a096e99ccf8e6a2b389aa1e
Clauudaw <Claudiardaw@gmail.com>
2025-01-09 15:45:43 +0100

    actualizations of config 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; // Importación de path
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080
+    port: 8080,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'), // Uso de path correctamente importado
+    },
+  },
+});
